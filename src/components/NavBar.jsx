8f0f25018d7833912d300003477986dd2bc4a098
João Lucas <joaolucaspinheirotorres@gmail.com>
2023-09-06 23:23:38 -0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -35,7 +35,7 @@ const NavBar = () => {
   return (
     <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed z-50'>
       <div>
-        <h1 className='text-5xl font-signature ml-2'>João Lucas</h1>
+        <h1 className='text-3xl font-bold ml-2'>JL</h1>
       </div>
       <ul className='hidden md:flex'>
         {links.map(({ id, link }) => (
